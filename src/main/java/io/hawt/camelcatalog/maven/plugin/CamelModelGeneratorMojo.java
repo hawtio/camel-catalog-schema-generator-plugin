883a58d6e7d9e66fd2993d20f325ff2cf957018d
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2023-03-30 12:25:55 +0100

    fix: Ensure that the camel-catalog dependency is resolved
    * If the mojo is executed in a phase prior to dependency resolution then
      the file location of the camel-catalog artifact will be null.
    
    * Ensures the requiresDependencyResolution property has been set to
      mandate the camel-catalog dependency to be resolved.

--- src/main/java/io/hawt/camelcatalog/maven/plugin/CamelModelGeneratorMojo.java
+++ src/main/java/io/hawt/camelcatalog/maven/plugin/CamelModelGeneratorMojo.java
@@ -10,12 +10,14 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
 /**
  * To generate camelModel.js from the Apache Camel release
  */
-@Mojo(name = "generate-camel-model", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+@Mojo(name = "generate-camel-model", defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+            requiresDependencyResolution = ResolutionScope.COMPILE)
 public class CamelModelGeneratorMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
@@ -39,6 +41,10 @@ public class CamelModelGeneratorMojo extends AbstractMojo {
             return;
         }
         getLog().info("Using Apache Camel " + camelCatalog.getVersion());
+        if (camelCatalog.getFile() == null) {
+            throw new MojoFailureException("Camel Catalog dependency has not yet been resolved");
+        }
+        getLog().info("Camel Catalog Artifact location: " + camelCatalog.getFile().getAbsolutePath());
 
         CamelModelGenerator generator = new CamelModelGenerator(getLog(), camelCatalog.getVersion(),
                 camelCatalog.getFile(), schemaFile);
